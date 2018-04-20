bf368a69ffed2ba1055686b46952aa99c4f216f1
Atticus Pomerantz <rapomerantz@gmail.com>
2018-04-20 16:14:38 -0500

    Add wireframe done

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Card, { CardContent } from 'material-ui/Card';
+
+
 
 class Add extends Component {
   render() {
     return (
-      <div >
+      <div className="container">
         <h1>ADD RECOLLECTIONS VIEW</h1>
+
+        <Card className="formContainer">
+          <CardContent>
+          <form>
+          <h3>Topic</h3>
+          <input type="text" name="topic"/>
+
+          <h3>Reflection</h3>
+          <input type="text" name="reflection"/>
+
+          <input type="submit"/>
+
+
+          </form>
+          </CardContent>
+
+
+        </Card>
+
       </div>
     );
   }
--- src/components/Recollections/Recollections.js
+++ src/components/Recollections/Recollections.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Recollections.css'
+import Card, { CardContent } from 'material-ui/Card';
+import Button from 'material-ui/Button'; 
 
 class Recollections extends Component {
   render() {
     return (
-      <div >
+      <div className="container">
         <h1>RECOLLECTIONS VIEW</h1>
+        <Card className="recollectionCard">
+          <CardContent>
+            <h3>I'm a recollection</h3>
+            <h4>Date of recollection goes here</h4>
+            <p>Subcontent of recollection goes here</p>
+            <Button className="cardButton">Delete Recolletion</Button>
+            <Button className="cardButton">Bookmark Recollection</Button>
+          </CardContent>
+        </Card>
+        <Card className="recollectionCard">
+          <CardContent>
+            <h3>I'm a recollection</h3>
+            <h4>Date of recollection goes here</h4>
+            <p>Subcontent of recollection goes here</p>
+            <Button className="cardButton">Delete Recolletion</Button>
+            <Button className="cardButton">Bookmark Recollection</Button>
+          </CardContent>
+        </Card>
       </div>
     );
   }
